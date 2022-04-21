65d42a160519df671a052a2359643fc21744a5a6
CelesteJack <97849551+CelesteJack@users.noreply.github.com>
2022-04-21 15:47:47 -0400

    credentials
    added credentials for my coding game base

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,8 @@
-
+"""
+An implementation of Minimax AI Algorithm in Tic Tac Toe,
+using Python.
+This software is available under GPL license.
+Author: Clederson Cruz
+Year: 2017
+License: GNU GENERAL PUBLIC LICENSE (GPL)
+"""
